fab809ff4e8f457137e85f519b94b3a69b2a82e3
Jahaira <jahaira.marrero@gmail.com>
2021-03-29 17:11:52 -0400

    we tried and failed with css

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,28 @@
 import React from "react"
+import CardDeck from 'react-bootstrap/CardDeck'
+import Card from 'react-bootstrap/Card'
 
-function Recipe({name,ingredients,instructions,image}){
+function Recipe({name,ingredients,image}){
     return(
-        <div>
-        <h3>{name}</h3>
-        <img src={image} alt={name}/>
-        <h3>{ingredients}</h3>
-        <p>{instructions}</p>
-        </div>
+        <CardDeck style={{width: 'flex', flexDirection: 'row'}}>
+        
+         <Card style={{width: 2, height: 2}} >
+          <Card.Img  variant="top" src={image} alt={name} />
+    <Card.Body style={{width: 1, height: 1}} >
+      <Card.Title>{name}</Card.Title>
+      <Card.Text>
+        {ingredients}
+      </Card.Text>
+        </Card.Body>
+         </Card>
+        
+    </CardDeck>
+        // <div>
+        // <h3>{name}</h3>
+        // <img src={image} alt={name}/>
+        // <h3>{ingredients}</h3>
+        // <p>{instructions}</p>
+        // </div>
     )
 }
 export default Recipe
--- src/components/User.js
+++ src/components/User.js
@@ -1,4 +1,6 @@
 import React from "react"
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 // import Header from "./Header"
 
 function User({name, image, restrictions, diet}) {
@@ -18,15 +20,26 @@ function User({name, image, restrictions, diet}) {
   console.log(image)
     
 return (
-    <div>
-        <img src={image} alt={name}/>
-        <h3>{name}</h3>
-        <h3>Restrictions: {restrictions}</h3> 
-        <h3>Preference: {diet}</h3>
-        <>
+
+  <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={image} alt={name} />
+  <Card.Body>
+    <Card.Title>{name}</Card.Title>
+    <Card.Text>
+      {diet}
+    </Card.Text>
+    <Button variant="primary">{restrictions}</Button>
+  </Card.Body>
+</Card>
+//     <div>
+//         <img src={image} alt={name}/>
+//         <h3>{name}</h3>
+//         <h3>Restrictions: {restrictions}</h3> 
+//         <h3>Preference: {diet}</h3>
+//         <>
   
-</>
-    </div>
+// </>
+//     </div>
     )
 }
 export default User
